974fccb888ed854150038394c076855c8ff92c03
Stephen_Lai <stephenlai2021@gmail.com>
2022-10-29 16:39:35 +0800

    commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+const http = require('http')
+
+const twilio = require('twilio');
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+app.get('/api/get-turn-credentials', (req, res) => {
+  const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+  
+  client.tokens.create().then(token => {
+    console.log('turn server credentials: ', token)
+    res.json({ token })
+  }).catch(error => {
+    console.log(error)
+    res.send({ message: 'failed to fetch TURN crendntials', error })
+  })
+})
+
+server.listen(PORT, () => console.log(`server listening on port ${PORT}`))
